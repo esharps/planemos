2700fbe15da71d2a1c0ab259c804b4e3c53d997f
Emily Sharp <emily.codes@gmail.com>
2015-09-15 08:05:21 -0400

    Added a spacepong v1 scene with Sam's 3D assets

--- Pong Tutorial 3D/Assets/My Scripts/PlayerPaddle.cs
+++ Pong Tutorial 3D/Assets/My Scripts/PlayerPaddle.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class PlayerPaddle : MonoBehaviour {
 
 	public float paddleSpped = 1.0f;
-
 	private Vector3 playerPos = new Vector3(20, 0, 0);
 
-	
+	public Transform slidingPaddle;
+	private Vector3 targetPos;
+
+#if UNITY_EDITOR
 	// Update is called once per frame
 	void Update () {
 
@@ -15,4 +17,16 @@ public class PlayerPaddle : MonoBehaviour {
 		playerPos = new Vector3 (20, Mathf.Clamp (yPos, -9, 9), 0);
 		transform.position = playerPos;
 	}
+#endif
+	/**void Start() {
+		targetPos = slidingPaddle.position;
+	}
+
+	void Update() {
+		slidingPaddle.position = Vector3.Lerp (slidingPaddle.position, targetPos, Time.deltaTime * 7);
+	}
+
+	void OnTouchStay(Vector3 point) {
+		targetPos = new Vector3 (targetPos.x, point.y, targetPos.z);
+	}**/
 }
