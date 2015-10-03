d3e2ae15d95f545c4001104c1a27c8e78abb84a7
emsharp <emsharp@ncsu.edu>
2015-10-03 15:56:57 -0400

    Commit to branch

---
+++ Pong Tutorial 3D/Assets/My Scripts/ScoreManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static int playerScore;
+	public static int enemyScore;
+
+	Text playerScoreText;
+	Text enemyScoreText;
+	
+	void Awake () {
+
+		playerScore = 0;
+		playerScoreText = GetComponent<Text> ();
+		playerScoreText.text = "" + playerScore;
+
+		enemyScore = 0;
+		enemyScoreText = GetComponent<Text> ();
+		enemyScoreText.text = "" + enemyScore;
+	}
+	
+
+	void Update () {
+		playerScoreText.text = "" + playerScore;
+		enemyScoreText.text = "" + enemyScore;
+	}
+
+	// If the enemy scores on the player, increment enemy score
+	void OnEnemyScore() {
+		enemyScore++;
+		enemyScoreText.text = "" + enemyScore;
+	}
+
+	// If the player scores on the enemy, increment player score
+	void OnPlayerScore() {
+		playerScore++;
+		playerScoreText.text = "" + playerScore;
+	}
+
+}
