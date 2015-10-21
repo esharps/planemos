f05d53cfba03ab4f788511fd146b2342a2f014b4
emsharp <emsharp@ncsu.edu>
2015-10-21 12:34:31 -0400

    Imported a Unity Asset package containing different sci-fi effects

---
+++ SpacePong/Assets/CorsairGames/SpaceRebel/Effects/Fx/Reward/FxAnimationCurve.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class FxAnimationCurve : FxAnimation {
+	// === Unity ======================================================================================================
+	public AnimationCurve Curve;
+}
