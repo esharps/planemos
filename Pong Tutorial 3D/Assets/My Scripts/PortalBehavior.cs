4db8ca9836a4464e366bfcb5858692df640a7b67
Jon Jackson <jdjacks7@ncsu.edu>
2015-10-22 13:44:29 -0400

    working on portals

--- Pong Tutorial 3D/Assets/My Scripts/PortalBehavior.cs
+++ Pong Tutorial 3D/Assets/My Scripts/PortalBehavior.cs
@@ -5,33 +5,59 @@ public class PortalBehavior : MonoBehaviour {
 
 	private GameObject orange;
 	private GameObject purple;
-	public bool transporting;
+	//public bool transporting;
+	private bool portalsActive = true;
+	private long timer = 0;
 
 	void Start(){
 		orange = transform.FindChild ("Orange").gameObject;
 		purple = transform.FindChild ("Purple").gameObject;
 	}
+
+
 	public void OnOrangeCollision(Collider c){
-		purple.GetComponent<BoxCollider>().enabled = false;
+		orange.GetComponent<BoxCollider> ().enabled = false;
 		moveToPurple (c.gameObject);
 	}
 
 	public void OnPurpleCollision(Collider c){
 		orange.GetComponent<BoxCollider> ().enabled = false;
+		purple.GetComponent<BoxCollider>().enabled = false;
 		moveToOrange (c.gameObject);
 	}
+
+//	public void onTriggerEnter(Collider c){
+//		moveToPurple(c)
+//		portalsActive = false;
+//	}
 
 	void moveToPurple(GameObject go){
 		if(go.CompareTag("Ball")){
 			go.transform.position = purple.transform.position;
-			purple.GetComponent<BoxCollider>().enabled = true;
+//			purple.GetComponent<BoxCollider>().enabled = true;
 		}
 	}
 
 	void moveToOrange(GameObject go){
 		if(go.CompareTag("Ball")){
 			go.transform.position = orange.transform.position;
-			orange.GetComponent<BoxCollider>().enabled = true;
+//			orange.GetComponent<BoxCollider>().enabled = true;
+		}
+	}
+
+	public void update(){
+		timer += Time.deltaTime;
+		if (timer > 1) {
+			timer = 0;
+			if(!portalsActive){
+				activatePortals();
+			}
+		}
+	}
+
+	public void activatePortals(){
+		for(Transform child in transform){
+			child.GetComponent<BoxCollider>().enabled = true;
 		}
 	}
 }
