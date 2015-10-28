fbd92805bca5a89832f000e7d2719e5ad9861fe5
snhoang <snhoang@ncsu.edu>
2015-10-27 21:09:17 -0400

    Implemented rotation of title model
    Made it so that players could swipe the title's model and make it spin
    for fun. This was basically a test to see how to get the  menu options
    rotation to work.

--- Planemos/Assets/My Scripts/TitleRotate.cs
+++ Planemos/Assets/My Scripts/TitleRotate.cs
@@ -1,16 +1,30 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;using UnityEngine;using System.Collections;public class TitleRotate : MonoBehaviour {    // For when the player touches the title    private float rotSpeed = 10.0f;    private float lerpSpeed = 1.0f;    private Vector3 speed;    private Vector3 avgSpeed;    private bool dragging = false;    private Vector3 targetSpeedX;    // When player's not touching the title    private float idleRotSpeed = 6.0f;
+    private float idleTime = 0.0f;
 
-public class TitleRotate : MonoBehaviour {
-
-    float rot_speed = 4;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Rotate(0, rot_speed * Time.deltaTime, 0);
-	}
-}
+    void OnMouseDown()
+    {
+        dragging = true;
+    }		void Update ()
+    {
+        if (Input.GetMouseButton(0) && dragging)
+        {
+            speed = new Vector3(Input.GetAxis("Mouse X"), 0.0f, 0.0f);
+            avgSpeed = Vector3.Lerp(avgSpeed, speed, Time.deltaTime * 5); // What's the 5 for?
+        }
+        else
+        {
+            if (dragging)
+            {
+                speed = avgSpeed;                
+                idleTime = Time.time;
+                dragging = false;
+            }
+            float i = Time.deltaTime * lerpSpeed;
+            speed = Vector3.Lerp(speed, Vector3.zero, i);
+        }
+         
+        if (Time.time - idleTime > 5 || idleTime == 0.0f)
+            transform.Rotate(0.0f, idleRotSpeed * Time.deltaTime, 0.0f);
+        else
+            transform.Rotate(0.0f, speed.x * rotSpeed, 0.0f);
+    }}
