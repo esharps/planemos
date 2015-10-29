7a3d6b2eaf19d65b9476dcb69654d0c96124dcbe
snhoang <snhoang@ncsu.edu>
2015-10-29 14:46:59 -0400

    Added in menu icons
    Included menu icons for the main menu and stuck the rotation script I
    made for the title model onto the menu icons. Menu icons can now rotate
    now! I just have to figure out how to make it so that it'll stop
    automatically when it comes close to one of the icons

--- Planemos/Assets/My Scripts/Main Menu Scripts/MainMenuRotate.cs
+++ Planemos/Assets/My Scripts/Main Menu Scripts/MainMenuRotate.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class MainMenuRotate : MonoBehaviour {
+    
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // For when the player touches the menu
+    private float rotSpeed = 10.0f;    private float lerpSpeed = 1.0f;    private Vector3 speed;    private Vector3 avgSpeed;    private bool dragging = false;
+
+    // When player's not touching the title
+    private float idleRotSpeed = 6.0f;    private float idleTime = 0.0f;    void OnMouseDown()    {        dragging = true;    }
+
+    void Update () {
+
+        if (ChangeShader.started)        {            if (Input.GetMouseButton(0) && dragging)            {                speed = new Vector3(Input.GetAxis("Mouse X"), 0.0f, 0.0f);                avgSpeed = Vector3.Lerp(avgSpeed, speed, Time.deltaTime * 5); // What's the 5 for?
+            }            else            {                if (dragging)                {                    speed = avgSpeed;                    idleTime = Time.time;                    dragging = false;                }                float i = Time.deltaTime * lerpSpeed;                speed = Vector3.Lerp(speed, Vector3.zero, i);            }            if (Time.time - idleTime > 5 || idleTime == 0.0f)                transform.Rotate(0.0f, -(idleRotSpeed * Time.deltaTime), 0.0f);            else                transform.Rotate(0.0f, -(speed.x * rotSpeed), 0.0f);        }
+
+    }
 }
