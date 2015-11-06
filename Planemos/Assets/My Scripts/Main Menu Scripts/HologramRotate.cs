62bffed62f3f7d79e1400ea3b8a2b970ad4a7c71
snhoang <snhoang@ncsu.edu>
2015-11-05 20:23:37 -0500

    Added 2D sprite of hologram base
    Added a sprite that will fade into view as the projector "powers up" for
    added aesthetics. Also tried adding in functionality for when the player
    taps on a menu option, the camera would rotate in the direction it needs
    to go depending on the option. It doesn't work at the moment, because I
    don't have colliders on the  menu options just yet. I'm not sure if I
    have to add those the hard way though.

---
+++ Planemos/Assets/My Scripts/Main Menu Scripts/HologramRotate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HologramRotate : MonoBehaviour {
+
+    public GameObject outer;
+    public GameObject inner;
+
+    private float startTime = 0.0f;
+    private float duration = 10.0f;
+    private SpriteRenderer rendOuter;
+    private SpriteRenderer rendInner;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+        rendOuter = outer.GetComponent<SpriteRenderer>();
+        rendInner = inner.GetComponent<SpriteRenderer>();    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (MainMenuStartup.rays)
+        {
+            rendOuter.color = new Color(1f, 1f, 1f, Mathf.SmoothStep(-0.1f, 1.0f, (Time.time - startTime) / duration));
+            rendInner.color = new Color(1f, 1f, 1f, Mathf.SmoothStep(-0.1f, 1.0f, (Time.time - startTime) / duration));
+        }
+
+        outer.transform.Rotate(0.0f, 0.0f, 0.2f);
+        inner.transform.Rotate(0.0f, 0.0f, -0.2f);
+	}
+}
