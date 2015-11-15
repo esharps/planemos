ee9310344efa2c755b4325babf5677e089e4418f
emsharp <emsharp@ncsu.edu>
2015-11-15 11:09:29 -0500

    Added scripting behavior for button clicks

--- Planemos/Assets/My Scripts/TestFlight Scripts/ButtonManagerTF.cs
+++ Planemos/Assets/My Scripts/TestFlight Scripts/ButtonManagerTF.cs
@@ -1,21 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 // Manages all behavior that can occur as the result
 // of clicking a UI-based button object via OnClick()
 public class ButtonManagerTF : MonoBehaviour {
-
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 
 	// OnClick() NEXT button in tutorial
 	public void PressNext() {
@@ -29,6 +18,8 @@ public class ButtonManagerTF : MonoBehaviour {
 	
 	// OnClick() Quit
 	public void QuitGame() {
+
+		//NOTE: QUIT only works after project is built and run
 		Application.Quit ();
 	}
 }
