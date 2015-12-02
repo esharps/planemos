e3c59f3c321dcc1d068a83e3d763c4c136720b2c
emsharp <emsharp@ncsu.edu>
2015-12-02 15:07:33 -0500

    Used a very thin plane to create a depth tracking line

---
+++ Planemos/Assets/My Scripts/TestFlight Scripts/DepthLineTF.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DepthLineTF : MonoBehaviour {
+
+	public static float zPosition;
+
+	// Use this for initialization
+	void Start () {
+		zPosition = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (transform.position.z);
+	}
+}
